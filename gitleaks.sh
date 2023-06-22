8234857ffa60a0105e3f6cde4a62e80e4ad88891
Adalbert Barta <adalbertbarta@gmail.com>
2023-06-22 03:12:02 +0200

    Add Gitleaks installation and usage guide

--- gitleaks.sh
+++ gitleaks.sh
@@ -12,17 +12,16 @@ if [ "$(id -u)" != "0" ] && [ "$(uname -o)" != "Msys" ]; then
     exit 1
 fi
 
-
-# Install or uninstall pre-commit hook based on the argument
-if [ "$1" == "in" ]; then
+# Check if the pre-commit hook is already installed
+if [ -f .git/hooks/pre-commit ]; then
+    echo -e "${YELLOW}Pre-commit hook is already installed. Removing it...${RESET_COLOR}"
+    sudo rm -f .git/hooks/pre-commit
+    echo -e "${GREEN}Pre-commit hook removed successfully.${RESET_COLOR}"
+else
+    echo -e "${GREEN}Pre-commit hook is not installed. Installing it...${RESET_COLOR}"
     curl -sSfL https://raw.githubusercontent.com/bartaadalbert/leaks/master/pre-commit.sh > .git/hooks/pre-commit
     # wget -O .git/hooks/pre-commit https://raw.githubusercontent.com/bartaadalbert/tf-pro/main/pre-commit.sh
     sudo chmod +x .git/hooks/pre-commit
     echo -e "${GREEN}Pre-commit hook installed successfully.${RESET_COLOR}"
-elif [ "$1" == "rm" ]; then
-    sudo rm -f .git/hooks/pre-commit
-    echo -e "${GREEN}Pre-commit hook uninstalled successfully.${RESET_COLOR}"
-else
-    echo -e "${YELLOW}Please specify '+' to install or '-' to uninstall.${RESET_COLOR}"
-    exit 1
 fi
+
